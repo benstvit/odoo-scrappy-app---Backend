1c9dfe924b69a9fcd6f58d58f5311646bcea6d39
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-11-02 12:07:42 +0100

    fix typo in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,50 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
   skip_before_action :authenticate_request, only: %i[create]
-end
 
-# GET /users
-def index
-  @users = User.all
-  render json: @users
-end
-
-# GET /users/id
-def show
-  render json: @user
-end
-
-# POST /users
-def create
-  @user = User.new(user_params)
-
-  if @user.save
-    render json: @user, status: :created, location: @user
-  else
-    render json: @user.errors, status: unprocessable_entity
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users
   end
-end
 
-# PUT /users/id
-def update
-  if @user.udpate(user_params)
+  # GET /users/id
+  def show
     render json: @user
-  else
-    render json: @user.errors, status: unprocessable_entity
   end
-end
 
-# DELETE /users/id
-def destroy
-  @user.destroy
-end
+  # POST /users
+  def create
+    @user = User.new(user_params)
 
-private
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: unprocessable_entity
+    end
+  end
 
-def set_user
-  @user = User.find(params[:id])
-end
+  # PUT /users/id
+  def update
+    if @user.udpate(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: unprocessable_entity
+    end
+  end
 
+  # DELETE /users/id
+  def destroy
+    @user.destroy
+  end
 
-def user_params
-  ActionController::Parameters.new(JSON.parse(params.require(:user))).permit(:email, :name, :password, :street, :zip)
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    ActionController::Parameters.new(JSON.parse(params.require(:user))).permit(:email, :name, :password, :street, :zip)
+  end
 end

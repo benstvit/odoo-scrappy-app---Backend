5ca66e6c2fd1e4d98de163ccba3c7a3eb2cbbb81
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-11-06 16:17:01 +0100

    remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'signin/create'
   namespace :api do
     namespace :v1 do
       resources :clients, only: [:index, :create, :destroy]
       resources :users
     end
   end
-
-  root to: "home#index"
 end

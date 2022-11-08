dae13b7fbc77c0dacbfa622d97e5f78322fea2e2
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-11-08 10:20:27 +0100

    remove useless worker settings puma

--- config/puma.rb
+++ config/puma.rb
@@ -18,21 +18,12 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
-
 preload_app!
 
 rackup      DefaultRackup if defined?(DefaultRackup)
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
 # port ENV.fetch('PORT', 3000)
 
 # Specifies the `environment` that Puma will run in.

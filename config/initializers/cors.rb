d02608fcdad3a22f5c0ed15175fb4f0c9ed9ef91
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-11-09 11:02:17 +0100

    update orgins cors to deployed website

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080' # Replace with prod URL
+    origins 'https://benstvit.github.io/odoo-front/' # Replace with prod URL
 
     resource '*',
       headers: :any,
